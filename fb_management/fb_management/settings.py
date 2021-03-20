04fc1352a42cf1975723c24a4eea245ee5a99390
idreesakkadan <idreesakkadan@gmail.com>
2021-03-20 22:40:07 +0530

    login file designed and rendered

--- fb_management/fb_management/settings.py
+++ fb_management/fb_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'social_django.middleware.SocialAuthExceptionMiddleware',  # <-- Configuration of Django Social Auth App
+    
+
 ]
 
 ROOT_URLCONF = 'fb_management.urls'
@@ -65,6 +70,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'social_django.context_processors.backends',  # <-- 
+                'social_django.context_processors.login_redirect', # <--
             ],
         },
     },
@@ -130,5 +138,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_URL = 'logout'
+LOGOUT_REDIRECT_URL = 'login'
+
--- fb_management/fb_management/urls.py
+++ fb_management/fb_management/urls.py
@@ -18,5 +18,9 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.urls'))
+    path('',include('core.urls')),
+    path('auth/', include('social_django.urls', namespace='social')),  # <--the social-auth-app-django URLs:
+
+
+
 ]
